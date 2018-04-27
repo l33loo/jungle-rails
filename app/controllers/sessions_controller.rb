760bd9f150bec6c8ea51ac35012cc2f36ddc107b
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-04-27 21:20:22 +0000

    Complete conditional for 'create' action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,15 @@
 class SessionsController < ApplicationController
 
   def new
+
   end
 
   def create
     @user = User.find_by_email(params[:email])
     if @user && @user.authenticate(params[:password])
       session[:id] = @user.id
+      redirect_to [:root]
+    else
       redirect_to [:root]
     end
   end
