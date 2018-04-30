7e2dad8a58700ef2d9b8fc6cfc2dfaf3357ddae4
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-04-30 20:57:22 +0000

    Add test that fails validation with nil name

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -14,18 +14,5 @@ RSpec.describe Product, type: :model do
       expect(prod.errors.full_messages).to include("Name can't be blank")
     end
 
-    it "fails validation with no price, expecting a specific error message" do
-      prod = Product.create(name: "Lala", price: nil, quantity: 3, category_id: 3)
-
-      expect(prod.errors.full_messages).to include("Price can't be blank")
-    end
-
-    it "fails validation with no quantity, expecting a specific error message" do
-      prod = Product.create(name: "Foo", price: 1200, quantity: nil, category_id: 1)
-    end
-
-    it "fails validation with no category, expecting a specific error message" do
-      prod = Product.create(name: "Joe", price: 1400, quantity: 12, category_id: nil)
-    end
   end
 end
