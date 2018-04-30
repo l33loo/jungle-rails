d56157d61bbf8988f73fdc1fbbcd829ee9767b62
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-04-30 18:04:24 +0000

    Add create action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,20 @@
 class ReviewsController < ApplicationController
-  def create
 
+  def create
+    @product = Product.find(params[:id])
+    @review = @product.reviews.new(review_params)
+    redirect_to [:root], notice: 'Review successfully posted!'
+
+  #   # if @review.save
+  #   #   redirect_to [@product], notice: 'Review successfully posted!'
+  #   # else
+  #   #   error
+  #   # end
+  # end
+
+  private
+
+  def review_params
+    params.require(:product).permit(:description, :rating)
   end
 end
