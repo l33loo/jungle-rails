e19a1f662db2407b1c96dd0c7a89bd8abd452829
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-05-01 01:03:01 +0000

    Update validation that new user is created with password and password confirmation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,12 +4,11 @@ RSpec.describe User, type: :model do
   describe 'EmailValidations' do
 
     it 'validates that a new user is created with a password and a password confirmation' do
-      user = User.create
-
-      expect(:password && :password_confirmation).not_to eq nil
+      user = User.create(first_name: 'Leelee', last_name: 'Sudoku', email: 'leelee@leelee.com')
+      expect(user).not_to be_valid
     end
-
   end
+
 end
 
 
