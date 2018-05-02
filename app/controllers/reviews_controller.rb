ced312dbd77d570c03ae3cf3a3c816f3e01bfc33
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-05-02 03:01:05 +0000

    Fix create action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,19 +2,20 @@ class ReviewsController < ApplicationController
 
   def create
     @product = Product.find(params[:product_id])
-    @review = @product.reviews.new(review_params)
-    redirect_to [:root], notice: 'Review successfully posted!'
+    review_params = params[:review]
+    review = Review.new(description: review_params[:description], rating: review_params[:rating], user_id: session[:id], product_id: params[:product_id])
+      puts "PARAAAAAMS #{review_params[:description]}"
 
-  #   # if @review.save
-  #   #   redirect_to [@product], notice: 'Review successfully posted!'
-  #   # else
-  #   #   error
-  #   # end
+    if review.save
+      redirect_to :back, notice: 'Review successfully posted!'
+    else
+      puts "error posting review"
+    end
   end
 
   private
 
-  def review_params
-    params.require(:review).permit(:description, :rating)
-  end
+  # def review_params
+  #   params.require(:review).permit(:description, :rating)
+  # end
 end
